0922200ed048f508cd294dff79429eb864d06159
berberAlla <alysik19842807@gmail.com>
2019-08-05 23:27:09 +0300

    created contact-list component

--- ui/contacts-ui/src/app/app.module.ts
+++ ui/contacts-ui/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppComponent } from './Components/root/app.component';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {HttpService} from './Services/http/http.service';
+import { ContactsListComponent } from './Components/contacts-list/contacts-list/contacts-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsListComponent
   ],
   imports: [
     BrowserModule,
